bfbb3b70afcb8e1e6595985e7bd0484cb808b6fa
Melissa Real <melissa.real@hotmail.com>
2019-01-04 12:13:20 +0000

    Automatically backed up by Learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,32 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.title = params[:title]
+    @student.description = params[:description]
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(title: params[:student][:title], description: params[:student][:description])
+    redirect_to student_path(@student)
+  end
+end
